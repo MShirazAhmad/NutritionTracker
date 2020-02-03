881bc3faa903d6d4f0a7f2be788a28d360cb1152
M. Shiraz Ahmad <35538879+MShirazAhmad@users.noreply.github.com>
2020-02-03 22:56:12 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,4 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'NT.ui'
-#
-# Created by: PyQt5 UI code generator 5.12.2
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
